ed4809ebee993119199ad187ce6ac16f607599e4
S Reddy <sachitgreddy@email.com>
2021-03-06 01:43:05 -0500

    Pushing changes so far to check how md file looks

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,15 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+  apiKey: "AIzaSyC2loWDaNS7FFKymeq3ryppOMPQ6B_BURI",
+    authDomain: "kiei451-final-452f4.firebaseapp.com",
+    projectId: "kiei451-final-452f4",
+    storageBucket: "kiei451-final-452f4.appspot.com",
+    messagingSenderId: "223326209430",
+    appId: "1:223326209430:web:c6b5db5a314ed1a63b49b7",
+    measurementId: "G-N8N9FWBJ5S"
+} // replaced
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
